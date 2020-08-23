78bf4c3cd894a47a9c98f733c5becc768be0c8d9
Marius Margowski <mariusmargowksi89@gmail.com>
2020-08-23 17:55:31 +0200

    Version 1.0

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,24 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+const getData = require('./getData');
+const renderData = require('./renderData');
 
 // Home page route.
-router.get('/', function (req, res) {
-  res.send('Hello World. Learn <a href="/about">about this app</a>');
-})
+router.get('/', async (req, res) => {
+  const apiUrl = "https://api.covid19api.com/summary";
+  try {
+    const data = await getData(apiUrl);
+    renderData(res, data, null, 'home', 'COVID-19 Cases');
+  } catch (err) {
+    console.error(err);    
+    renderData(res, null, err, 'home', 'COVID-19 Cases');
+  }
+});
 
 // About page route.
 router.get('/about', function (req, res) {
-  res.send('About this COVID-19 Tracker. Go <a href="/">home</a>');
-})
+  renderData(res, null, null, 'about', 'About');
+});
 
 module.exports = router;
