0b3408a3e63c26aab1c3840e7f386765a7d270fd
Jedsada Tiwongvorakul <jedsada.t@20scoops.net>
2020-09-25 14:29:42 +0700

    reformat code and handle error

--- src/getData.spec.js
+++ src/getData.spec.js
@@ -1,10 +1,10 @@
 const renderData = (res, data, error, tpl, pageName) => {
-    res.render(tpl, {
-        appName: "COVID-19 Tracker",
-        pageName,
-        data: data ? data : null,
-        error: error ? error : null,
-    });
-}
+  res.render(tpl, {
+    appName: "COVID-19 Tracker",
+    pageName,
+    data: data ? data : null,
+    error: error ? error : null,
+  });
+};
 
 module.exports = renderData;
